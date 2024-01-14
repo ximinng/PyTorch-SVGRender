c32fea18f3bdb0c91c9e7748306f95cd0453dbc8
Ximing Xing <ximingxing@gmail.com>
2024-01-14 13:01:44 +0800

    fix(setup): tip for pycharm to fix dependencies

--- pytorch_svgrender/__init__.py
+++ pytorch_svgrender/__init__.py
@@ -2,3 +2,5 @@
 # Author: ximing
 # Copyright (c) 2023, XiMing Xing.
 # License: MPL-2.0 License
+
+__version__ = "1.0"
--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='PyTorch-SVGRender',
-    packages=find_packages(exclude=["test*"]),
+    packages=find_packages(exclude=["test*", "docs"]),
     version='1.0.0',
     license='Mozilla Public License Version 2.0',
     description='SVG Differentiable Rendering: Generating vector graphics using neural networks.',
@@ -21,38 +21,43 @@ setup(
         'SVG',
     ],
     install_requires=[
-        'hydra-core',  # configuration processor
-        'omegaconf',  # YAML processor
-        'accelerate',  # Hugging Face - pytorch distributed configuration
+        'hydra-core==1.3.2',  # configuration processor
+        'omegaconf==2.3.0',  # YAML processor
+        'accelerate==0.20.3',  # Hugging Face - pytorch distributed configuration
         'diffusers==0.20.2',  # Hugging Face - diffusion models
-        'transformers',  # Hugging Face - transformers
-        'safetensors',
+        'transformers==4.30.2',  # Hugging Face - transformers
+        'datasets==2.13.1',
+        'safetensors==0.3.1',
         'xformers',
-        'einops',
+        'einops==0.6.1',
         'pillow',
+        'imageio-ffmpeg==0.4.8'
         'torch>=1.13.1',
-        'torchvision',
-        'tensorboard',
-        'torchmetrics',
-        'triton',
-        'numba',
+        'torchvision>=0.14.1',
+        'tensorboard==2.14.0',
+        'triton==2.0.0.post1',
+        'numba==0.57.1',
         'tqdm',  # progress bar
-        'ftfy',
-        'regex',
-        'timm',  # computer vision models
-        "numpy",  # numpy
-        'scikit-learn',
-        'scikit-fmm',
-        'scipy',
-        'scikit-image',
+        'ftfy==6.1.1',
+        'regex==2023.6.3',
+        'timm==0.6.13',  # computer vision models
+        "numpy==1.24.4",
+        'scikit-learn==1.3.2',
+        'scikit-fmm==2023.4.2',
+        'scipy==1.10.1',
+        'scikit-image==0.20.0',
         'Pillow',  # keep the PIL.Image.Resampling deprecation away,
-        'matplotlib',
-        'visdom',
-        'wandb',  # weights & Biases
-        'opencv-python',  # cv2
-        'BeautifulSoup4',
+        'pytorch-lightning==2.1.0',
+        'matplotlib==3.7.1',
+        'visdom=0.2.4',
+        'wandb==0.15.8',  # weights & Biases
+        'opencv-python==4.8.0.74',  # cv2
+        'BeautifulSoup4==4.12.2',
         'freetype-py',  # font
-        'shapely',  # SVG
+        'shapely==2.0.1',  # SVG
+        'svgwrite==1.4.3',
+        'svgutils==0.3.4',
+        'svgpathtools==1.6.1',
         'fairscale=0.4.13'  # ImageReward
     ],
     dependency_links=[
