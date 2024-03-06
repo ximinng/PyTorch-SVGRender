0fcc62e8c08c982194f95b1d415764d3b046e03e
Ximing Xing <ximingxing@gmail.com>
2024-03-06 20:18:28 +0800

    refactor(method): rename: clipdraw

---
+++ pytorch_svgrender/painter/clipdraw/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+# Author: ximing
+# Description: __init__.py
+# Copyright (c) 2024, XiMing Xing.
+# License: MPL-2.0 License
+
+from .painter_params import Painter, PainterOptimizer
