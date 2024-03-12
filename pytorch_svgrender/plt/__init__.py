a5a4865b71b1514454d4285db165a3d4ca90d487
Ximing Xing <ximingxing@gmail.com>
2024-03-12 23:21:34 +0800

    fix(visual): tight layout

--- pytorch_svgrender/plt/__init__.py
+++ pytorch_svgrender/plt/__init__.py
@@ -3,7 +3,9 @@
 # Copyright (c) 2023, XiMing Xing.
 # License: MPL-2.0 License
 
-from typing import AnyStr
+from typing import AnyStr, BinaryIO, Union
+from PIL import Image
+import pathlib
 
 import matplotlib.pyplot as plt
 import torch
@@ -14,22 +16,23 @@ def plot_couple(input_1: torch.Tensor,
                 input_2: torch.Tensor,
                 step: int,
                 output_dir: str,
-                fname: str,  # file name
+                fname: Union[str, pathlib.Path, BinaryIO],  # file name
                 prompt: str = '',  # text prompt as image tile
+                pad_value: float = 0,
                 dpi: int = 300):
     if input_1.shape != input_2.shape:
         raise ValueError("inputs and outputs must have the same dimensions")
 
     plt.figure()
     plt.subplot(1, 2, 1)  # nrows=1, ncols=2, index=1
-    grid = make_grid(input_1, normalize=True, pad_value=2)
+    grid = make_grid(input_1, normalize=True, pad_value=pad_value)
     ndarr = grid.mul(255).add_(0.5).clamp_(0, 255).permute(1, 2, 0).to("cpu", torch.uint8).numpy()
     plt.imshow(ndarr)
     plt.axis("off")
     plt.title("Input")
 
     plt.subplot(1, 2, 2)  # nrows=1, ncols=2, index=2
-    grid = make_grid(input_2, normalize=True, pad_value=2)
+    grid = make_grid(input_2, normalize=True, pad_value=pad_value)
     ndarr = grid.mul(255).add_(0.5).clamp_(0, 255).permute(1, 2, 0).to("cpu", torch.uint8).numpy()
     plt.imshow(ndarr)
     plt.axis("off")
@@ -54,27 +57,31 @@ def plot_couple(input_1: torch.Tensor,
 
 def plot_img(inputs: torch.Tensor,
              output_dir: AnyStr,
-             fname: str,  # file name
-             dpi: int = 100):
+             fname: Union[str, pathlib.Path, BinaryIO],  # file name
+             pad_value: float = 0):
     assert torch.is_tensor(inputs), f"The input must be tensor type, but got {type(inputs)}"
 
-    grid = make_grid(inputs, normalize=True, pad_value=2)
+    grid = make_grid(inputs, normalize=True, pad_value=pad_value)
     ndarr = grid.mul(255).add_(0.5).clamp_(0, 255).permute(1, 2, 0).to("cpu", torch.uint8).numpy()
+
     plt.imshow(ndarr)
     plt.axis("off")
     plt.tight_layout()
-    plt.savefig(f"{output_dir}/{fname}.png", dpi=dpi, bbox_inches='tight')
     plt.close()
+
+    im = Image.fromarray(ndarr)
+    im.save(f"{output_dir}/{fname}.png")
 
 
 def plot_img_title(inputs: torch.Tensor,
                    title: str,
                    output_dir: AnyStr,
-                   fname: str,  # file name
+                   fname: Union[str, pathlib.Path, BinaryIO],  # file name
+                   pad_value: float = 0,
                    dpi: int = 500):
     assert torch.is_tensor(inputs), f"The input must be tensor type, but got {type(inputs)}"
 
-    grid = make_grid(inputs, normalize=True, pad_value=2)
+    grid = make_grid(inputs, normalize=True, pad_value=pad_value)
     ndarr = grid.mul(255).add_(0.5).clamp_(0, 255).permute(1, 2, 0).to("cpu", torch.uint8).numpy()
     plt.imshow(ndarr)
     plt.axis("off")
